a78f860092f5b93f2f6d6948ad5505ad0a1bf60e
unknown <vitorribeiro9454@gmail.com>
2022-03-26 22:17:39 -0300

    tela de financiamento

--- app/Models/Financiamento.php
+++ app/Models/Financiamento.php
@@ -13,128 +13,180 @@ class Financiamento
 
         $resultado = ($valorEntrada*100)/$valorVei;
         
-        if( !empty($resultado) ) {
-            if($resultado >= 0 && $resultado <= 20) {
-            if($parcela >= 0 && $parcela <= 20) {
-                $valorVei = $valorVei -$valorEntrada;
-                $c = ($valorVei/$parcela);
-                $d = $c * 2.35;
-                $number = $d;
-                $number = number_format($number, 2, '.', '');}
-            else if($parcela >= 21 && $parcela <= 40){
-                $valorVei = $valorVei -$valorEntrada;
-                $c = ($valorVei/$parcela);
-                $d = $c * 2.40;
-                $number = $d;
-                $number = number_format($number, 2, '.', '');}
-            else if($parcela >= 41 && $parcela <= 60){
-                $valorVei = $valorVei -$valorEntrada;
-                $c = ($valorVei/$parcela);
-                $d = $c * 2.45;
-                $number = $d;
-                $number = number_format($number, 2, '.', '');}
-            }
-            else if($resultado >= 0.21 && $resultado <= 0.40){
-            if($parcela >= 0 && $parcela <= 20) {
-                $valorVei = $valorVei -$valorEntrada;
-                $c = ($valorVei/$parcela);
-                $d = $c * 2.20;
-                $number = $d;
-                $number = number_format($number, 2, '.', '');}
-            else if($parcela >= 21 && $parcela <= 40){
-                $valorVei = $valorVei -$valorEntrada;
-                $c = ($valorVei/$parcela);
-                $d = $c * 2.25;
-                $number = $d;
-                $number = number_format($number, 2, '.', '');}
-            else if($parcela >= 41 && $parcela <= 60){
-                $valorVei = $valorVei -$valorEntrada;
-                $c = ($valorVei/$parcela);
-                $d = $c * 2.30;
-                $number = $d;
-                $number = number_format($number, 2, '.', '');}
-            }
-            else if($resultado >= 41 && $resultado <= 60){
-            if($parcela >= 0 && $parcela <= 20) {
-                $valorVei = $valorVei -$valorEntrada;
-                $c = ($valorVei/$parcela);
-                $d = $c * 2.05;
-                $number = $d;
-                $number = number_format($number, 2, '.', '');}
-            else if($parcela >= 21 && $parcela <= 40){
-                $valorVei = $valorVei -$valorEntrada;
-                $c = ($valorVei/$parcela);
-                $d = $c * 2.10;
-                $number = $d;
-                $number = number_format($number, 2, '.', '');}
-            else if($parcela >= 41 && $parcela <= 60){
-                $valorVei = $valorVei -$valorEntrada;
-                $c = ($valorVei/$parcela);
-                $d = $c * 2.15;
-                $number = $d;
-                $number = number_format($number, 2, '.', '');}
-            }
-            else if($resultado >= 61 && $resultado <= 80){
-            if($parcela >= 0 && $parcela <= 20) {
-                $valorVei = $valorVei -$valorEntrada;
-                $c = ($valorVei/$parcela);
-                $d = $c * 1.9;
-                $number = $d;
-                $number = number_format($number, 2, '.', '');}
-            else if($parcela >= 21 && $parcela <= 40){
-                $valorVei = $valorVei -$valorEntrada;
-                $c = ($valorVei/$parcela);
-                $d = $c * 1.95;
-                $number = $d;
-                $number = number_format($number, 2, '.', '');}
-            else if($parcela >= 41 && $parcela <= 60){
-                $valorVei = $valorVei -$valorEntrada;
-                $c = ($valorVei/$parcela);
-                $d = $c * 2.0;
-                $number = $d;
-                $number = number_format($number, 2, '.', '');}
-            }
-            else if($resultado >= 81 && $resultado <= 90){
-            if($parcela >= 0 && $parcela <= 20) {
-                $valorVei = $valorVei -$valorEntrada;
-                $c = ($valorVei/$parcela);
-                $d = $c * 1.75;
-                $number = $d;
-                $number = number_format($number, 2, '.', '');}
-            else if($parcela >= 21 && $parcela <= 40){
-                $valorVei = $valorVei -$valorEntrada;
-                $c = ($valorVei/$parcela);
-                $d = $c * 1.80;
-                $number = $d;
-                $number = number_format($number, 2, '.', '');}
-            else if($parcela >= 41 && $parcela <= 60){
-                $valorVei = $valorVei -$valorEntrada;
-                $c = ($valorVei/$parcela);
-                $d = $c * 1.85;
-                $number = $d;
-                $number = number_format($number, 2, '.', '');}
-            }
-            else if($resultado >= 91 && $resultado <= 100){
-                if($parcela >= 0 && $parcela <= 20) {
+        if( !empty($resultado) ) 
+        {
+            if($resultado >= 0 && $resultado <= 20) 
+            {
+                if($parcela >= 0 && $parcela <= 20) 
+                {
                     $valorVei = $valorVei -$valorEntrada;
                     $c = ($valorVei/$parcela);
-                    $d = $c * 1.6;
-                    $number = $d;
-                    $number = number_format($number, 2, '.', '');}
-                else if($parcela >= 21 && $parcela <= 40){
+                    $d = $c * 2.35;
+                    $resultadoFinanciamento = $d;
+                    $resultadoFinanciamento = number_format($resultadoFinanciamento, 2, '.', '');
+                    echo "O resultado da operação é: $resultadoFinanciamento";                }
+                else if($parcela >= 21 && $parcela <= 40)
+                {
                     $valorVei = $valorVei -$valorEntrada;
                     $c = ($valorVei/$parcela);
-                    $d = $c * 1.65;
-                    $number = $d;
-                    $number = number_format($number, 2, '.', '');}
+                    $d = $c * 2.40;
+                    $resultadoFinanciamento = $d;
+                    $resultadoFinanciamento = number_format($resultadoFinanciamento, 2, '.', '');
+                }
+                else if($parcela >= 41 && $parcela <= 60)
+                {
+                    $valorVei = $valorVei -$valorEntrada;
+                    $c = ($valorVei/$parcela);
+                    $d = $c * 2.45;
+                    $resultadoFinanciamento = $d;
+                    $resultadoFinanciamento = number_format($resultadoFinanciamento, 2, '.', '');
+                }
+            }
+
+
+            else if($resultado >= 21 && $resultado <= 40)
+            {
+                if($parcela >= 0 && $parcela <= 20) 
+                {
+                    $valorVei = $valorVei -$valorEntrada;
+                    $c = ($valorVei/$parcela);
+                    $d = $c * 2.20;
+                    $resultadoFinanciamento = $d;
+                    $resultadoFinanciamento = number_format($resultadoFinanciamento, 2, '.', '');
+                }
+                else if($parcela >= 21 && $parcela <= 40)
+                {
+                    $valorVei = $valorVei -$valorEntrada;
+                    $c = ($valorVei/$parcela);
+                    $d = $c * 2.25;
+                    $resultadoFinanciamento = $d;
+                    $resultadoFinanciamento = number_format($resultadoFinanciamento, 2, '.', '');
+                }
                 else if($parcela >= 41 && $parcela <= 60){
                     $valorVei = $valorVei -$valorEntrada;
                     $c = ($valorVei/$parcela);
-                    $d = $c * 1.70;
-                    $number = $d;
-                    $number = number_format($number, 2, '.', '');}
+                    $d = $c * 2.30;
+                    $resultadoFinanciamento = $d;
+                    $resultadoFinanciamento = number_format($resultadoFinanciamento, 2, '.', '');
                 }
-            return $number;
+            }
+
+
+            else if($resultado >= 41 && $resultado <= 60)
+            {
+                if($parcela >= 0 && $parcela <= 20) 
+                {
+                    $valorVei = $valorVei -$valorEntrada;
+                    $c = ($valorVei/$parcela);
+                    $d = $c * 2.05;
+                    $resultadoFinanciamento = $d;
+                    $resultadoFinanciamento = number_format($resultadoFinanciamento, 2, '.', '');
+                }
+                else if($parcela >= 21 && $parcela <= 40)
+                {
+                    $valorVei = $valorVei -$valorEntrada;
+                    $c = ($valorVei/$parcela);
+                    $d = $c * 2.10;
+                    $resultadoFinanciamento = $d;
+                    $resultadoFinanciamento = number_format($resultadoFinanciamento, 2, '.', '');
+                }
+                else if($parcela >= 41 && $parcela <= 60)
+                {
+                    $valorVei = $valorVei -$valorEntrada;
+                    $c = ($valorVei/$parcela);
+                    $d = $c * 2.15;
+                    $resultadoFinanciamento = $d;
+                    $resultadoFinanciamento = number_format($resultadoFinanciamento, 2, '.', '');
+                }
+            }
+
+
+            else if($resultado >= 61 && $resultado <= 80)
+            {
+                if($parcela >= 0 && $parcela <= 20) 
+                {
+                    $valorVei = $valorVei -$valorEntrada;
+                    $c = ($valorVei/$parcela);
+                    $d = $c * 1.9;
+                    $resultadoFinanciamento = $d;
+                    $resultadoFinanciamento = number_format($resultadoFinanciamento, 2, '.', '');
+                }
+                else if($parcela >= 21 && $parcela <= 40)
+                {
+                    $valorVei = $valorVei -$valorEntrada;
+                    $c = ($valorVei/$parcela);
+                    $d = $c * 1.95;
+                    $resultadoFinanciamento = $d;
+                    $resultadoFinanciamento = number_format($resultadoFinanciamento, 2, '.', '');
+                }
+                else if($parcela >= 41 && $parcela <= 60)
+                {
+                    $valorVei = $valorVei -$valorEntrada;
+                    $c = ($valorVei/$parcela);
+                    $d = $c * 2.0;
+                    $resultadoFinanciamento = $d;
+                    $resultadoFinanciamento = number_format($resultadoFinanciamento, 2, '.', '');
+                }
+                }
+
+
+            else if($resultado >= 81 && $resultado <= 90)
+            {
+                if($parcela >= 0 && $parcela <= 20) 
+                {
+                    $valorVei = $valorVei -$valorEntrada;
+                    $c = ($valorVei/$parcela);
+                    $d = $c * 1.75;
+                    $resultadoFinanciamento = $d;
+                    $resultadoFinanciamento = number_format($resultadoFinanciamento, 2, '.', '');
+                }
+                else if($parcela >= 21 && $parcela <= 40)
+                {
+                    $valorVei = $valorVei -$valorEntrada;
+                    $c = ($valorVei/$parcela);
+                    $d = $c * 1.80;
+                    $resultadoFinanciamento = $d;
+                    $resultadoFinanciamento = number_format($resultadoFinanciamento, 2, '.', '');
+                }
+                else if($parcela >= 41 && $parcela <= 60)
+                {
+                    $valorVei = $valorVei -$valorEntrada;
+                    $c = ($valorVei/$parcela);
+                    $d = $c * 1.85;
+                    $resultadoFinanciamento = $d;
+                    $resultadoFinanciamento = number_format($resultadoFinanciamento, 2, '.', '');
+                }
+            }
+            
+            
+            else if($resultado >= 91 && $resultado <= 100)
+            {
+                if($parcela >= 0 && $parcela <= 20) 
+                {
+                    $valorVei = $valorVei -$valorEntrada;
+                    $c = ($valorVei/$parcela);
+                    $d = $c * 1.6;
+                    $resultadoFinanciamento = $d;
+                    $resultadoFinanciamento = number_format($resultadoFinanciamento, 2, '.', '');
+                }
+                else if($parcela >= 21 && $parcela <= 40)
+                {
+                    $valorVei = $valorVei -$valorEntrada;
+                    $c = ($valorVei/$parcela);
+                    $d = $c * 1.65;
+                    $resultadoFinanciamento = $d;
+                    $resultadoFinanciamento = number_format($resultadoFinanciamento, 2, '.', '');
+                }
+                else if($parcela >= 41 && $parcela <= 60)
+                {
+                    $valorVei = $valorVei -$valorEntrada;
+                    $c = ($valorVei/$parcela);
+                    $d = $c * 1.70;
+                    $resultadoFinanciamento = $d;
+                    $resultadoFinanciamento = number_format($resultadoFinanciamento, 2, '.', '');
+                }
+            }
+            return $resultadoFinanciamento;
         }
 
     }
--- resources/views/financiamento/form.blade.php
+++ resources/views/financiamento/form.blade.php
@@ -9,8 +9,6 @@
 
                 <div class="card-body">
 
-
-
                     <div class="card mb-3" style="max-width: 100%;">
                         <div class="row g-0">
                           <div class="col-md-4">
@@ -18,56 +16,36 @@
                           </div>
                           <div class="col-md-8">
                             <div class="card-body">
-                              <h5 class="card-title">{{$data->title}}</h5>
+                              <h5 class="card-title">{{$data->model}}</h5>
                               <h5 class="card-title">{{ __('Price')}}: R$ <span style="color: red">{{$data->price}}</span></h5>
-                              <p class="card-text">Vendedor: {{$data->user->name}}</p>
+                              <p class="card-text">{{ __('Type')}}: {{$data->type->name}} </p>
                               <p class="card-text">{{$data->description}}</p>
                               <p class="card-text"><small class="text-muted">{{$data->created_at}}</small></p>
                               <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-
-                                <a href="{{route("financiamento",$data)}}">
-                                    <button class="btn btn-primary me-md-2" type="button"> 
-                                        {{ __('View') }}
-                                    </button>
-                                </a>
-
-                                @can('delete',$data)  
-                                    <form action="{{route('advert.destroy',$data)}}" method="post">
-                                        @csrf
-                                            @method("DELETE")
-                                                <button class="btn btn-danger" type="button" onclick="confirmDeleteModal(this)" >
-                                                    {{ __('Delete') }}
-                                                </button>
-                                    </form>
-                                @endcan
                               </div>
                             </div>
                           </div>
                         </div>
-                      </div>
+                    </div>
 
 
-                        <form id="main" method="POST" action="{{ route('financiamento.calcular', $data) }}" enctype="multipart/form-data">
+                    <form id="main" method="POST" action="{{ route('financiamento.calcular', $data) }}" enctype="multipart/form-data">
 
                         @csrf
-
-                        Valor da entrada: <input name="valorEntrada" type="text"><br>
-                        Número de parcela: <input name="quantidadeParcela" type="text"><br>    
                         
-                        </form> 
-
-
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                    <button type="submit" id="btn-save" class="btn btn-primary" form="main">
-                                        {{ __('Calcular') }}
-                                    </button>
-                            </div>
+                        <div class="input-group mb-3">
+                            <label>Valor da entrada: <input name="valorEntrada" type="text" class="form-control" aria-describedby="basic-addon1"></label>
                         </div>
-                    
-
                         
+                        <div class="input-group mb-3">
+                               <label>Número de parcela: <input name="quantidadeParcela" type="text" placeholder="Max: 60" class="form-control" aria-describedby="basic-addon2"></label>
+                        </div>
+                        <div class="input-group-append">
+                            <button type="submit" id="btn-save" class="btn btn-primary" form="main">
+                                {{ __('Calcular') }}
+                            </button>
+                        </div>
+                    </form> 
                 </div>
             </div>
         </div>
